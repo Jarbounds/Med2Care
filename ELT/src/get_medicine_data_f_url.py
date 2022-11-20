f6c159ff4ae345ebe87155744bf846e95c7c45cd
Renato de Sousa Marcelo <61599300+Jarbounds@users.noreply.github.com>
2022-11-20 17:22:33 +0000

    Updates function to be more resilient

--- ELT/src/get_medicine_data_f_url.py
+++ ELT/src/get_medicine_data_f_url.py
@@ -1,21 +1,68 @@
+import re
+from Utils.utils import verify_word
+
+import jsonpickle
 import requests
 from requests import Response
-from bs4 import BeautifulSoup, Tag
-from medicine_dataclasses import Medicine
-
+from bs4 import BeautifulSoup, Tag, NavigableString
+from medicine_dataclasses import Medicine, Pregnancy
 
 default_medicine_url_file: str = '../data/medicines_urls.txt'
 
 
-def semicolon_separate_excipients(excipients_content: str) -> str:
-    excipient_content_parts: list = excipients_content.split('\n\n')
-    excipients_list_raw: list = [
-            excipients_str
-            for i, excipients_str in enumerate(excipient_content_parts) if i % 2 != 0
+# known_excipient_headers: list = ['tablet coating', 'tablet core']
+
+
+def semicolon_separate_excipients(excipients_tag: Tag) -> str:
+    excipients_list: list = []
+    for child in excipients_tag.children:
+        if isinstance(child, Tag):
+            for grand_child in child.children:
+                if isinstance(grand_child, NavigableString) and grand_child.isprintable():
+                    string: str = grand_child.text.strip()
+                    excipients_list.append(string)
+
+    to_ret = ';'.join(excipients_list)
+    return to_ret
+
+
+def process_preg_contraind(pregnancy_div: Tag) -> list[Pregnancy]:
+    # Separate pregnancy contraindications (fertility, pregnancy, lactation and others), if they exist
+    # Todo: See if the 'others' cannot fall in the pregnancy together instead of another object
+    names_descriptions: dict[str, str] = {}
+    current_description: str = ''
+    current_name: str = ''
+    for child in pregnancy_div.children:
+        if not isinstance(child, Tag):
+            continue
+
+        is_first_title: bool = True
+        for tag_child in child.children:
+            if isinstance(tag_child, Tag):
+                # is a name
+                if tag_child.name in ['i', 'u', 'b'] and is_first_title:
+                    if len(current_description) != 0:
+                        names_descriptions[current_name] = current_description.strip()
+                        current_description = ''
+                    current_name = tag_child.text
+                    is_first_title = False
+                elif not is_first_title:
+                    current_description = f'{current_description} {tag_child.text.strip()}'
+            elif isinstance(tag_child, NavigableString) and tag_child.isprintable():
+                if is_first_title:
+                    current_description = f'{current_description}\n{tag_child.text.strip()}'
+                else:
+                    current_description = f'{current_description} {tag_child.text.strip()}'
+
+    if len(current_description) != 0:
+        names_descriptions[current_name] = current_description.strip()
+
+    # Process lists to Pregnancy objects
+    pregnancy_contraindications: list = [
+        Pregnancy(name, description) for name, description in names_descriptions.items()
     ]
-    excipients_joined = ''.join(excipients_list_raw)
-    excipients_to_return = excipients_joined.replace(' \n', '\n').replace('\n', ';')
-    return excipients_to_return
+
+    return pregnancy_contraindications
 
 
 # Todo: Define a semicolon separate incompatibilities function, if seen necessary.
@@ -23,14 +70,16 @@ def semicolon_separate_excipients(excipients_content: str) -> str:
 
 
 def main():
-    with open(default_medicine_url_file, 'r') as url_files:
+    with open(default_medicine_url_file, 'r', encoding='utf-8') as url_files:
         medicine_urls: list = [url.rstrip('\n') for url in url_files.readlines()]
 
     for medicine_url in medicine_urls:
         response: Response = requests.get(medicine_url)
         soup = BeautifulSoup(response.content, features='html.parser')
+        medicine_id: str = re.findall("[0-9]+", medicine_url)[0]
 
         current_medicine: Medicine = Medicine()
+        current_medicine.medicine_id = medicine_id
 
         product_info_anchor = soup.find('a', attrs={'id': 'PRODUCTINFO'})
         for elem in product_info_anchor.next_elements:
@@ -64,8 +113,8 @@ def main():
         pregnancy_anchor = soup.find('a', attrs={'id': 'PREGNANCY'})
         for elem in pregnancy_anchor.next_elements:
             if isinstance(elem, Tag) and 'sectionWrapper' in elem.attrs['class']:
-                pregnancy: str = elem.text.strip('\n')
-                current_medicine.metadata.clinical_particulars.contraindications.pregnancy = pregnancy
+                pregnancy_contraindications: list[Pregnancy] = process_preg_contraind(elem)
+                current_medicine.metadata.clinical_particulars.contraindications.pregnancy = pregnancy_contraindications
                 break
 
         machine_ops_anchor = soup.find('a', attrs={'id': 'MACHINEOPS'})
@@ -78,8 +127,7 @@ def main():
         excipients_anchor = soup.find('a', attrs={'id': 'EXCIPIENTS'})
         for elem in excipients_anchor.next_elements:
             if isinstance(elem, Tag) and 'sectionWrapper' in elem.attrs['class']:
-                excipients: str = elem.text.strip('\n')
-                excipients_formatted = semicolon_separate_excipients(excipients)
+                excipients_formatted: str = semicolon_separate_excipients(elem)
                 current_medicine.metadata.clinical_particulars.contraindications.excipients = excipients_formatted
                 break
 
@@ -97,7 +145,10 @@ def main():
                 current_medicine.metadata.revision_date = revision_date
                 break
 
+        print(f'Writing file with id:{medicine_id}')
         # Todo: Save to file (missing the medicine id for complete definition)
+        with open(f'../data/json/{medicine_id}.json', 'w', encoding='utf-8') as file:
+            file.write(jsonpickle.encode(current_medicine, unpicklable=False, indent=4))
 
 
 if __name__ == '__main__':
