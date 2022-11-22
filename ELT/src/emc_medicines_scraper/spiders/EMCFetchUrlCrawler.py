eb60284813be7fcc3ee5af43a5b0f5ac4de57bf1
Renato de Sousa Marcelo <61599300+Jarbounds@users.noreply.github.com>
2022-11-22 19:13:07 +0000

    Changes implementation for use of Scrapy (on url fetching)
     Changes implementation for use of Scrapy instead of Beautiful Soup to be more robust (yet exponentially more difficult and time consuming to implement).

---
+++ ELT/src/emc_medicines_scraper/spiders/EMCFetchUrlCrawler.py
@@ -0,0 +1,69 @@
+from copy import deepcopy
+
+import scrapy
+import re
+from scrapy.http import Request, Response
+
+
+class EMCFetchUrlCrawler(scrapy.Spider):
+    name: str = 'EMCFetchUrlCrawler'
+    allowed_domains: list[str] = ['www.medicines.org.uk']
+
+    def is_discontinued(self, medicine_selector) -> bool:
+        r"""
+        Utility function for checking if a medicine entry is discontinued
+        :param medicine_selector: The selector that contains the medicine result.
+        """
+        icon_selector_list = medicine_selector.css('img')
+        if len(icon_selector_list) == 0:
+            return False
+        for attribute in icon_selector_list.attrib.values():
+            if 'discontinue' in attribute:
+                return True
+        return False
+
+    def start_requests(self):
+        # Request search for obtaining number of results
+        emc_search_url = self.kwargs.get('emc_search_url')
+        yield Request(emc_search_url, self.get_num_results, dont_filter=True)
+
+    def get_num_results(self, response: Response):
+        # Parse response
+        span_list = response.css('span')
+        total_results_span = [span for span in span_list if span.attrib.get('id', '') == 'SearchResultsPagingView'][0]
+        total_results = int(re.findall('[0-9]+', total_results_span.root.text)[0])
+
+        current_offset: int = 1
+        limit = self.kwargs.get('limit')
+        emc_search_url = self.kwargs.get('emc_search_url')
+
+        urls = []
+
+        while True:
+            to_search_url = f'{emc_search_url}&offset={current_offset}&limit={limit}&fullText=true'
+            urls.append(deepcopy(to_search_url))
+
+            current_offset += limit
+            if current_offset > total_results:
+                break
+
+        for url in urls:
+            yield Request(url, callback=self.parse, dont_filter=True)
+
+    def parse(self, response: Response, **kwargs):
+        results_box = response.css('div.search-results')
+        results = results_box.css('div.data-row')
+        for result in results:
+            if self.is_discontinued(result):
+                continue
+            link_list_div = result.css('div.col-sm-3')[0]
+            link_list = link_list_div.css('ul')[0]
+            links = link_list.css('li')
+            for link in links:
+                text: str = link.css('a::text').get()
+                if text.lower() == 'smpc':
+                    yield {
+                        'url': link.css('a::attr(href)').get()
+                    }
+                    break
+
--- ELT/src/get_medicines_url.py
+++ ELT/src/get_medicines_url.py
@@ -1,49 +1,28 @@
-import requests
-from bs4 import BeautifulSoup, Tag
-from requests import Response
+from scrapy.crawler import Crawler, CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from scrapy import signals
+from emc_medicines_scraper.spiders import EMCFetchUrlCrawler
 
-SmPC_str: str = 'SmPC'
-default_export_file = '../data/medicines_urls.txt'
+DEFAULT_EXPORT_FILE = '../data/medicines_urls.txt'
+MAX_RESULT_SIZE = 200
 
 medicine_urls: list = []
 
-
-def is_discontinued(medicine_result_element: Tag) -> bool:
-    r"""
-    Utility function for checking if a medicine entry is discontinued
-    :param medicine_result_element: The Tag element that contains the medicine result.
-    """
-    discontinued_icon_element = medicine_result_element.find(name='img')
-    if discontinued_icon_element is None:
-        return False
-    for attribute in discontinued_icon_element.attrs.values():
-        if 'discontinue' in attribute:
-            return True
-    return False
+# EMC base page url
+medicines_base_url: str = 'https://www.medicines.org.uk'
 
 
-def iterate_search_results(search_results, base_link: str):
-    for result in search_results:
-        # Check if medicine is discontinued
-        if is_discontinued(result):
-            continue
-        # Find all the 'a' tags
-        a_tags = result.findAll(name='a')
-        # Filter tags to find the one that has the Summary of Product Characteristics (SmPC) aka Medicine Information
-        a_tags_filtered = list(filter(lambda a_tag: a_tag.string == SmPC_str, a_tags))
-        # If a SmPC 'a' tag is found
-        if len(a_tags_filtered) != 0:
-            # Extract the href attribute
-            href: str = a_tags_filtered[0].attrs['href']
-            # Create a complete url for the medicine SmPC
-            absolute_href = f'{base_link}{href}'
-            # Append to medicine list
-            medicine_urls.append(absolute_href)
+def handle_url_scraped(item: dict):
+    medicine_urls.append(f'{medicines_base_url}{item["url"]}')
 
 
 def main():
-    # EMC base page url
-    medicines_base_url: str = 'https://www.medicines.org.uk'
+    settings = get_project_settings()
+    process = CrawlerProcess(settings)
+
+    fetch_urls_crawler = Crawler(EMCFetchUrlCrawler, settings=settings)
+    fetch_urls_crawler.signals.connect(handle_url_scraped, signal=signals.item_scraped)
+
     # EMC base search url
     emc_base_search_url: str = f'{medicines_base_url}/emc/search'
     # Define the partial ATC Code for searching medicines (in this case for HIV)
@@ -51,38 +30,18 @@ def main():
     partial_atc_code_query: str = 'q=J05AF'
     # Show only medicines with health professional information
     healthcare_information_filter_query = 'filters=attributes[spc]'
-    # Concatenate all search criteria (excluding mutable offset)
-    emc_search_url: str = f'{emc_base_search_url}?{partial_atc_code_query}&{healthcare_information_filter_query}'
 
-    current_offset: int = 1  # Strangely offset starts in 1 (no offset)
-    limit: int = 50
-    while True:
-        to_search_url = f'{emc_search_url}&offset={current_offset}&limit={limit}&fullText=true'
-        # Request search for atc code
-        response: Response = requests.get(to_search_url)
-        # Parse response
-        soup = BeautifulSoup(response.content, features='html.parser')
-        # Find search results elements
-        search_results_elem = soup.find(
-            name='div',
-            attrs={
-                'class': 'search-results'
-            },
-        )
-        if search_results_elem is None:
-            break
-        # Filter non Tag elements (e.g. Navigable Strings)
-        results_raw = [result for result in search_results_elem.children if isinstance(result, Tag)]
-        if len(results_raw) == 0:
-            break
-        # Iterate over the result elements
-        iterate_search_results(results_raw, medicines_base_url)
-        if len(results_raw) != limit:
-            break
-        current_offset += limit
+    process.crawl(fetch_urls_crawler, kwargs={
+        'emc_search_url': f'{emc_base_search_url}?{partial_atc_code_query}&{healthcare_information_filter_query}',
+        'base_offset': 1,
+        'limit': 200
+    })
+
+    process.start()
+    process.join()
 
     # write urls to file
-    with open(default_export_file, 'w', encoding='utf-8') as file:
+    with open(DEFAULT_EXPORT_FILE, 'w', encoding='utf-8') as file:
         for url in medicine_urls:
             file.write(f'{url}\n')
 
