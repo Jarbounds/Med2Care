726282de34c760c90ff39317e4beac38c7060d27
Renato de Sousa Marcelo <61599300+Jarbounds@users.noreply.github.com>
2022-11-23 15:25:25 +0000

    Removes unused dataclasses

--- ELT/src/medicine_dataclasses/Pregnancy.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass, field
-
-
-@dataclass
-class Pregnancy:
-    name: str = field(init=True, default_factory=str)
-    description: str = field(init=True, default_factory=str)
