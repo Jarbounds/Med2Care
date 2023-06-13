88bedf2e12dc601a6b1961a0b3f6227a3b49b81f
Renato de Sousa Marcelo <renato.marcelo99@gmail.com>
2023-06-13 16:03:11 +0100

    Updates column names

--- KB/src/dump.py
+++ KB/src/dump.py
@@ -20,33 +20,33 @@ if __name__ == '__main__':
     dump_database(
         'entities_sim_med2care_rs',
         'norm_similarity_chebi_sim_jc',
-        'comp_1,comp_2,sim_jc,l2,zscore,\'min-max\',tanh,\'log-sig\''
+        'comp_1,comp_2,sim_jc,l2,zscore,`min-max`,tanh,`log-sig`'
     )
 
     print('Dumping norm_similarity_chebi_sim_lin table')
     dump_database(
         'entities_sim_med2care_rs',
         'norm_similarity_chebi_sim_lin',
-        'comp_1,comp_2,sim_lin,l2,zscore,\'min-max\',tanh,\'log-sig\''
+        'comp_1,comp_2,sim_lin,l2,zscore,`min-max`,tanh,`log-sig`'
     )
 
     print('Dumping norm_similarity_chebi_sim_resnik table')
     dump_database(
         'entities_sim_med2care_rs',
         'norm_similarity_chebi_sim_resnik',
-        'comp_1,comp_2,sim_resnik,l2,zscore,\'min-max\',tanh,\'log-sig\''
+        'comp_1,comp_2,sim_resnik,l2,zscore,`min-max`,tanh,`log-sig`'
     )
 
     print('Dumping norm_similarity_structural_chebi_sim_morgan table')
     dump_database(
         'entities_sim_med2care_rs',
         'norm_similarity_structural_chebi_sim_morgan',
-        'comp_1,comp_2,sim_morgan,l2,zscore,\'min-max\',tanh,\'log-sig\''
+        'comp_1,comp_2,sim_morgan,l2,zscore,`min-max`,tanh,`log-sig`'
     )
 
     print('Dumping norm_similarity_structural_chebi_sim_tanimoto table')
     dump_database(
         'entities_sim_med2care_rs',
         'norm_similarity_structural_chebi_sim_tanimoto',
-        'comp_1,comp_2,sim_tanimoto,l2,zscore,\'min-max\',tanh,\'log-sig\''
+        'comp_1,comp_2,sim_tanimoto,l2,zscore,`min-max`,tanh,`log-sig`'
     )
