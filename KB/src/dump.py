6a84ec425b827c1c5d4043c168c4fe9198d5b585
Renato de Sousa Marcelo <renato.marcelo99@gmail.com>
2023-06-25 16:02:26 +0100

    Updates dump database to have database definition via config and in file names

--- KB/src/dump.py
+++ KB/src/dump.py
@@ -1,52 +1,57 @@
 from utils.utils2database import dump_database
+from utils.myconfiguration import MyConfiguration as Config
 
 
 if __name__ == '__main__':
+    config: Config = Config.get_instance()
+
+    database_name = config.database
+
     print('Dumping similarity_chebi table')
     dump_database(
-        'entities_sim_med2care_rs',
+        database_name,
         'similarity_chebi',
         'comp_1,comp_2,sim_resnik,sim_lin,sim_jc'
     )
 
     print('Dumping similarity_structural_chebi table')
     dump_database(
-        'entities_sim_med2care_rs',
+        database_name,
         'similarity_structural_chebi',
         'comp_1,comp_2,sim_tanimoto,sim_morgan'
     )
 
     print('Dumping norm_similarity_chebi_sim_jc table')
     dump_database(
-        'entities_sim_med2care_rs',
+        database_name,
         'norm_similarity_chebi_sim_jc',
         'comp_1,comp_2,sim_jc,l2,zscore,`min-max`,tanh,`log-sig`'
     )
 
     print('Dumping norm_similarity_chebi_sim_lin table')
     dump_database(
-        'entities_sim_med2care_rs',
+        database_name,
         'norm_similarity_chebi_sim_lin',
         'comp_1,comp_2,sim_lin,l2,zscore,`min-max`,tanh,`log-sig`'
     )
 
     print('Dumping norm_similarity_chebi_sim_resnik table')
     dump_database(
-        'entities_sim_med2care_rs',
+        database_name,
         'norm_similarity_chebi_sim_resnik',
         'comp_1,comp_2,sim_resnik,l2,zscore,`min-max`,tanh,`log-sig`'
     )
 
     print('Dumping norm_similarity_structural_chebi_sim_morgan table')
     dump_database(
-        'entities_sim_med2care_rs',
+        database_name,
         'norm_similarity_structural_chebi_sim_morgan',
         'comp_1,comp_2,sim_morgan,l2,zscore,`min-max`,tanh,`log-sig`'
     )
 
     print('Dumping norm_similarity_structural_chebi_sim_tanimoto table')
     dump_database(
-        'entities_sim_med2care_rs',
+        database_name,
         'norm_similarity_structural_chebi_sim_tanimoto',
         'comp_1,comp_2,sim_tanimoto,l2,zscore,`min-max`,tanh,`log-sig`'
     )
